50609c6caf7294ef2549098e022165b4fe48e66e
mrnicky28 <lavrinovich_nikita@mail.ru>
2021-12-10 18:49:16 +0300

    fix

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+//Install express server
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-app.use(express.static(".dist/budget-app"));
+// Serve only the static files form the dist directory
+app.use(express.static("./dist/budget-app"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/angular-heroku/" })
+  res.sendFile("index.html", { root: "dist/budget-app/" })
 );
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
